5b17e54345040dcd4698a7721f358dc19c3be7d5
Fuad010 <fuadtopchiyev@gmail.com>
2024-12-26 00:13:03 +0400

    feat:update dependency injection

--- Notes.Application/obj/Debug/net5.0/Notes.Application.AssemblyInfo.cs
+++ Notes.Application/obj/Debug/net5.0/Notes.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d2f814dc09f449e1887d6ff5a42bb1304b995e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8bdf63e9ccf09f8adb729ec650a69fdbcec422")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
